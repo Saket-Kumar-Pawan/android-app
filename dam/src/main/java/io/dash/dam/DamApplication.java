3aad37a892cf61dec5739751f9b60b4126d72715
Saket-Kumar-Pawan <126236422+Saket-Kumar-Pawan@users.noreply.github.com>
2023-09-01 10:33:22 +0530

    first commit

---
+++ dam/src/main/java/io/dash/dam/DamApplication.java
@@ -0,0 +1,13 @@
+package io.dash.dam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamApplication.class, args);
+	}
+
+}
---
+++ dam/src/test/java/io/dash/dam/DamApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dash.dam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
